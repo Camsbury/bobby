118a5c64507e71cc16db837acfda1ed43c7585ca
Cameron Kingsbury <camsbury7@gmail.com>
2018-09-16 19:51:12 -0400

    feat: add `Game` `Square` and `Move` types

--- src/Bobby/Type/Game.hs
+++ src/Bobby/Type/Game.hs
@@ -2,28 +2,34 @@
 {-# LANGUAGE TemplateHaskell       #-}
 --------------------------------------------------------------------------------
 -- |
--- Module: Bobby.Type.Move
--- Description: Provides the 'Move' type and accessors
+-- Module: Bobby.Type.Game
+-- Description: Provides the 'Game' type and accessors
 -- Maintainers: Cameron Kingsbury <camsbury7@gmail.com>
 -- Maturity: Draft
 --
 --------------------------------------------------------------------------------
-module Bobby.Type.Move
-  ( module Bobby.Type.Move
+module Bobby.Type.Game
+  ( module Bobby.Type.Game
   ) where
 --------------------------------------------------------------------------------
 import Prelude
 --------------------------------------------------------------------------------
 import Control.Lens (makeFieldsNoPrefix)
 --------------------------------------------------------------------------------
+import Bobby.Type.Board (Board)
 import Bobby.Type.Square (Square)
+import Bobby.Type.Color (Color)
 --------------------------------------------------------------------------------
 
-data Move
-  = Move
-  { _source :: Square
-  , _target :: Square
+data Game
+  = Game
+  { _board :: Board
+  , _playerTurn :: Color
+  , _whiteKingSideCastle :: Bool
+  , _whiteQueenSideCastle :: Bool
+  , _blackKingSideCastle :: Bool
+  , _blackQueenSideCastle :: Bool
+  , _enPassantSquare :: Square
   }
-  deriving (Eq, Show)
 
-makeFieldsNoPrefix ''Move
+makeFieldsNoPrefix ''Game
